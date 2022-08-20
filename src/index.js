f0211e543b808f91fefdee8435a5a3bab21ca297
Sarikalikhar11 <sarikalikhar2619@gmail.com>
2022-08-20 13:08:13 +0530

    change

--- src/index.js
+++ src/index.js
@@ -6,11 +6,16 @@ import cors from 'cors'
 dotenv.config('../.env')
 
 const app = express()
+app.use(express.static('public'));
 app.use(cors())
 app.use(express.json())
 const PORT = process.env.PORT || 8080
 
-app.get('/',(req,res)=>res.send(`Server is running on port ${PORT}`))
+app.get('/',(req,res)=>res.send(`Server is running on port ${PORT}Auth page (CI/CD)`))
+
+app.get('/login',(req,res)=>res.send(`This is login page`))
+
+app.get('/auth',(req,res)=>res.sendFile(__dirname+'/public/auth.html'))
 
 app.listen(PORT,()=>{
     console.log(`Server is running on port ${PORT} `)
